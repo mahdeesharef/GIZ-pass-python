41bd433ef389f539a9726a72862fee0d472acba8
mahdeesharef <71808546+mahdeesharef@users.noreply.github.com>
2021-09-29 01:40:40 +0300

    slution

--- python-pass.py
+++ python-pass.py
@@ -4,4 +4,14 @@ class Solution:
 
     @staticmethod
     def longest_palindromic(s: str) -> str:
-        pass
+        m_len = 0
+        m_word = ""
+        for first in range(len(s)):
+            for second in range(first+1, len(s)+1):
+                word = s[first:second]
+                if (word == word[::-1]):
+                    if m_len < len(word):
+                        m_len = len(word)
+                        m_word = word
+        return m_word
+        
